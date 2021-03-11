1d8e618d623c7642fb18f14014fcb1eaf58fdf82
sbbtt <sbbt89@naver.com>
2021-03-11 23:59:16 +0900

    minor changes

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
 import { Drawer, Button, Icon } from 'antd';
 import './Sections/Navbar.css';
-const Logo = require('../../../assets/images/HappyTubeLogo.png');
+const Logo = require('../../../assets/images/PetTubeLogo.png');
 
 function NavBar() {
   const [visible, setVisible] = useState(false)
